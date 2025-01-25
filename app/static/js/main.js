a398cca4c2b6da57e104ab8609b7401b60f30386
maRT <martin.foka@gmail.com>
2025-01-25 05:56:19 +0100

    Add loading indicator for better HTMX integration
    - Added a template `core/loading.html` to act as a visual indicator for HTMX requests.
    - Adjusted templates to support the new loading functionality.
    - Updated `main.js` to include a section for handling the progress bar animation during HTMX requests.
    - Adjusted and refined input CSS for improved visuals and compatibility.
    - Upgraded HTMX to version 2 for enhanced functionality and features.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,13 +3,48 @@
 // - When the dark/light mode state changes, it updates the `localStorage` to persist the user's preference.
 document.addEventListener('alpine:init', () => {
     Alpine.data('themeManager', () => ({
-        darkMode: localStorage.getItem('theme')
-            ? localStorage.getItem('theme') === 'dark'
-            : window.matchMedia('(prefers-color-scheme: dark)').matches,
+        // Determine the initial theme
+        darkMode: localStorage.getItem('theme') === 'dark'
+            || (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches),
+
+        // Save the theme preference whenever it changes
         init() {
-            this.$watch('darkMode', (value) => {
-                localStorage.setItem('theme', value ? 'dark' : 'light');
+            this.$watch('darkMode', (isDarkMode) => {
+                localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
             });
         },
     }));
+});
+
+
+// This section handles the progress bar animation for HTMX requests.
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the loading bar element for manipulation and progress variable
+    const loadingBar = document.getElementById('loading');
+    let progress = 0;
+
+    // Initializes the progress bar to 0% and starts the animation for a new HTMX request.
+    function initializeProgressBar() {
+        progress = 0;
+        loadingBar.style.width = '0%';
+        animateProgressBar();
+    }
+
+    // Animates the progress bar with random incremental progress until it reaches 80%.
+    function animateProgressBar() {
+        if (progress < 80) {
+            progress = Math.min(progress + Math.random() * 40, 80);
+            loadingBar.style.width = `${progress}%`;
+            setTimeout(animateProgressBar, 250);
+        }
+    }
+
+    // Completes the progress bar by setting it to 100% when the HTMX request is done.
+    function finalizeProgressBar() {
+        loadingBar.style.width = `100%`;
+    }
+
+    // Attach event listeners for HTMX lifecycle events.
+    document.body.addEventListener('htmx:beforeRequest', initializeProgressBar);  // Before request starts.
+    document.body.addEventListener('htmx:afterRequest', finalizeProgressBar); // After request completes.
 });
